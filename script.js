b24d67e924753e6c07ac7fdb52eb2e947c613b1e
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-07-25 20:45:29 -0400

    Com seleção de itens

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function selecionarPratoPrincipal(elemento) {
+    const marcado = document.querySelector(".selecionado");
+    const icone = document.querySelector(".selecionado .check");
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado");
+        icone.classList.add("oculto");                                          
+    } 
+    elemento.classList.add("selecionado")
+    const marcar = document.querySelector(".selecionado .check");
+    marcar.classList.remove("oculto");
+}  
 
+function selecionarBebida(elemento) {
+    const marcado = document.querySelector(".bebidas .selecionado");
+    const icone = document.querySelector(".bebidas .selecionado .check");    
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado");
+        icone.classList.add("oculto");                                           
+    } 
+
+    elemento.classList.add("selecionado")
+    const marcar = document.querySelector(".bebidas .selecionado .check");
+    marcar.classList.remove("oculto");
+}
+
+function selecionarSobremesa(elemento) {
+    const marcado = document.querySelector(".sobremesas .selecionado");
+    const icone = document.querySelector(".sobremesas .selecionado .check");    
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado");
+        icone.classList.add("oculto");                                          
+    } 
+    elemento.classList.add("selecionado")
+    const marcar = document.querySelector(".sobremesas .selecionado .check");
+    marcar.classList.remove("oculto");
+}
